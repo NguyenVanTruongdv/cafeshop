774bf2870df69ae58b52f8d65b7e8fa11d70f3b3
NguyenVanTruongdv <truong0236klj@gmail.com>
2026-04-09 23:02:07 +0700

    Fix conflict App.jsx
    Removed old App component structure and added new imports.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './routes/AppRouter';
-import { CartProvider } from './context/CartContext';
 
-function App() {
-  return (
-    <CartProvider>
-      <Router>
-        <AppRouter />
-      </Router>
-    </CartProvider>
-  );
-}
-
-export default App;
-=======
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Map from "./components/Map";
