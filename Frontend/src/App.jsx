eed58ce598b631bfd0036f97a2873898970450d9
Nguyen Nhat Hao <nhathao3011nt@gmail.com>
2026-04-10 15:08:19 +0700

    delete app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './routes/AppRouter';
-import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext';
 
-function App() {
-  return (
-    <AuthProvider>
-      <CartProvider>
-        <Router>
-          <AppRouter />
-        </Router>
-      </CartProvider>
-    </AuthProvider>
-  );
-}
-
-export default App;
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Map from "./components/Map";
